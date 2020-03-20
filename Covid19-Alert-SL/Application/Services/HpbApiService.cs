879751070e1a641eea67dc38a105fbb957fe2450
Francis Manoj Fernando <manoj@taloslogy.com>
2020-03-20 13:47:41 +0530

    init

---
+++ Covid19-Alert-SL/Application/Services/HpbApiService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services
+{
+    public class HpbApiService
+    {
+        IConfiguration Configuration { get; set; }
+        public HpbApiService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+    }
+}
---
+++ Covid19-Alert-SL/Scheduler/Program.cs
@@ -0,0 +1,29 @@
+﻿using Application.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Scheduler
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            
+            var config = GetConfiguration();
+
+            HpbApiService HbpApiService = new HpbApiService(config);
+            
+            Console.WriteLine("CLI Running");
+        }
+
+        private static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"cli.appsettings.json", false, true);
+            return builder.Build();
+        }
+
+    }
+}
