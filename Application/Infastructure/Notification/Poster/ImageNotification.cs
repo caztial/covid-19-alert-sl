fccac80097faff5f27dbd5f51c80b78097dbcbd7
Francis Manoj Fernando <manoj@taloslogy.com>
2020-03-29 09:20:57 +0530

    Added Death Count to the Image

--- Application/Infastructure/Notification/Poster/ImageNotification.cs
+++ Application/Infastructure/Notification/Poster/ImageNotification.cs
@@ -30,22 +30,25 @@ namespace Application.Infastructure.Notification.Poster
                
                 String dateAndTime = hpbStatistic.LastUpdate.ToString("dd.MM.yyyy") + " - " + hpbStatistic.LastUpdate.ToString("hh.mm tt");
 
-                image.Mutate(ctx => ctx.DrawText(dateAndTime, new Font(fontFamily, 60, FontStyle.Bold), Color.White, new PointF(50, 275)));
+                image.Mutate(ctx => ctx.DrawText(dateAndTime, new Font(fontFamily, 60, FontStyle.Bold), Color.White, new PointF(50, 230)));
 
                 image.Mutate(ctx => ctx.DrawText(hpbStatistic.LocalTotalCases.ToString(), 
-                                new Font(fontFamily, 80, FontStyle.Bold), Color.FromRgb(210, 9, 61), new PointF(40, 440)));
+                                new Font(fontFamily, 80, FontStyle.Bold), Color.FromRgb(210, 9, 61), new PointF(40, 350)));
                 
                 image.Mutate(ctx => ctx.DrawText("New Cases : "+ hpbStatistic.LocalNewCases.ToString(),
-                                new Font(fontFamily, 25, FontStyle.Bold), Color.FromRgb(210, 9, 61), new PointF(30, 600)));
+                                new Font(fontFamily, 25, FontStyle.Bold), Color.FromRgb(210, 9, 61), new PointF(30, 430)));
 
                 image.Mutate(ctx => ctx.DrawText(hpbStatistic.LocalActiveCases.ToString(),
-                                new Font(fontFamily, 80, FontStyle.Bold), Color.FromRgb(210, 9, 61), new PointF(40, 720)));
+                                new Font(fontFamily, 80, FontStyle.Bold), Color.FromRgb(210, 9, 61), new PointF(40, 610)));
 
                 image.Mutate(ctx => ctx.DrawText(hpbStatistic.LocalTotalNumberOfIndividualsInHospitals.ToString(),
-                                new Font(fontFamily, 80, FontStyle.Bold), Color.FromRgb(210, 9, 61), new PointF(40, 1025)));
+                                new Font(fontFamily, 80, FontStyle.Bold), Color.FromRgb(210, 9, 61), new PointF(40, 850)));
 
                 image.Mutate(ctx => ctx.DrawText(hpbStatistic.LocalRecoverd.ToString(),
-                                new Font(fontFamily, 90, FontStyle.Bold), Color.ForestGreen, new PointF(70, 1300)));
+                                new Font(fontFamily, 90, FontStyle.Bold), Color.ForestGreen, new PointF(80, 1110)));
+
+                image.Mutate(ctx => ctx.DrawText(hpbStatistic.LocalDeaths.ToString(),
+                                new Font(fontFamily, 90, FontStyle.Bold), Color.FromRgb(210, 9, 61), new PointF(80, 1355)));
 
                 String imageName = "status_update_" + hpbStatistic.Id + ".jpg";
 
